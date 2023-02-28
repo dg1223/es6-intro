e96f748e5053b1342056524260b2796c8aec2ccc
dg1223 <alavi1223@hotmail.com>
2023-02-28 01:56:33 -0500

    lasttt

--- js/callback.js
+++ js/callback.js
@@ -1,6 +1,19 @@
-function greeting(person) {
-  console.log(person);
+function greeting(greetingHandler, name) {
+  greetingHandler(name);
+}
+
+function greetingHandler(name) {
+  console.log("Good morning", name);
 }
 
 const name = "Halim mama";
-greeting(name);
+
+greeting(greetingHandler, name);
+greeting(greetingHandler, "Tom Brady");
+greeting(greetingHandler, "Tom Cruise");
+
+function greetEvening(name) {
+  console.log("Good evening", name);
+}
+
+greeting(greetEvening, "Tom Solaiman");
